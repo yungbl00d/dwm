ed09075911e994bae3f118b24a9991fb58b6c8f9
theo <trschrader@gmail.com>
2021-01-09 10:44:01 -0500

    applies focusonclick patch

--- config.def.h
+++ config.def.h
@@ -11,6 +11,7 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FontAwesome:size=12", "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static const int focusonwheel       = 0;
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
